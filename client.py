d193b6e06e543dcb9e111cc2815371e2969acd0b
cocampbe <campbco@gmail.com>
2017-06-08 15:07:06 -0500

    added get_tdev_dev_name

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 """
+
   symcli client to help assist in creating snapshot
  
   :author: Court Campbell <cwcampbell@eprod.com>
@@ -44,6 +45,27 @@ class symcli_client(object):
     for elem in tdevtree.getiterator('Device'):
       tdevs.append(elem.find('Dev_Info/dev_name').text)
     return tdevs
+  
+
+  def get_tdev_size(self,dev_name):
+    """ return tdev size as list. Sizes in MB, GB, TB. """
+    tdev_sizes = []
+    symdev = Popen(['symdev', '-sid', self.sid, 'list', '-devs', dev_name], stdout=PIPE, stderr=PIPE)
+    symdev_xml = ''.join(symdev.stdout.readlines())
+    tdevtree = ET.fromstring(symdev_xml)
+    for elem in tdevtree.getiterator('Capacity'):
+      tdev_sizes.append(elem.find('megabytes').text)
+      tdev_sizes.append(elem.find('gigabytes').text)
+      tdev_sizes.append(elem.find('terabytes').text)
+    return tdev_sizes
+
+
+  def get_tdev_dev_name(self,dev_name):
+    """ return tdev device name """
+    symdev = Popen(['symdev', '-sid', self.sid, 'list', '-identifier', 'device_name', '-devs', dev_name], stdout=PIPE)
+    symdev_xml = ''.join(symdev.stdout.readlines())
+    tdevtree = ET.fromstring(symdev_xml)
+    return tdevtree.find('Symmetrix/Device/Dev_Info/dev_ident_name').text
 
  
   def sg_create(self,sgname): 
@@ -58,16 +80,3 @@ class symcli_client(object):
     symconf = Popen(['symconfigure', '-sid', array_id, '-cmd', "create dev count=1,size=" + size + "mb,config=tdev,emulation=fba,preallocate size=all,sg=" + sg_name + ",device_name=" + tdev_name + ";", 'commit', '-nop'])
     stdout,stderr = symconf.communicate()
     print stdout or stderr
-
-
-  def get_tdev_size(self,dev_name):
-    """ return tdev size as list. Sizes in MB, GB, TB. """
-    tdev_sizes = []
-    symdev = Popen(['symdev', '-sid', self.sid, 'list', '-devs', dev_name], stdout=PIPE, stderr=PIPE)
-    symdev_xml = ''.join(symdev.stdout.readlines())
-    tdevtree = ET.fromstring(symdev_xml)
-    for elem in tdevtree.getiterator('Capacity'):
-      tdev_sizes.append(elem.find('megabytes').text)
-      tdev_sizes.append(elem.find('gigabytes').text)
-      tdev_sizes.append(elem.find('terabytes').text)
-    return tdev_sizes
